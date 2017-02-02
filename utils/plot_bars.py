2621e719f30c25ffde2a10a651d8c52eed4c8547
Yonatan Belinkov <belinkov@mit.edu>
2017-02-02 12:27:14 -0500

    plot difference in word vs char accuracy

--- utils/plot_bars.py
+++ utils/plot_bars.py
@@ -16,10 +16,10 @@ import numpy as np
 labels = ['Unseen', 'Seen', 'All']
 groups = ['POS', 'Morphology']
 sets = ['Word', 'Char']
-word_pos_accs = [37.93, 81.69, 78.20]
-char_pos_accs = [75.51, 93.95, 92.48]
-word_morph_accs = [17.21, 69.36, 65.20]
-char_morph_accs = [49.89, 82.24, 79.66]
+word_pos_accs = np.array([37.93, 81.69, 78.20])
+char_pos_accs = np.array([75.51, 93.95, 92.48])
+word_morph_accs = np.array([17.21, 69.36, 65.20])
+char_morph_accs = np.array([49.89, 82.24, 79.66])
 
 def autolabel(rects):
     # attach some text labels
@@ -30,7 +30,7 @@ def autolabel(rects):
                  '{}'.format(np.round(height, 1)),
                 ha='center', va='bottom')
 
-def plot_bars_two_sets(accs1, accs2, sets, labels, title, fignum, filename):
+def plot_bars_two_sets(accs1, accs2, sets, labels, title, fignum, filename, auto_label=True):
     """ bar plot comparing two sets of results """
     
     assert len(accs1) == len(accs2) and len(accs2) == len(labels), 'incompatible arguments in plot_bars_two_sets'
@@ -45,20 +45,82 @@ def plot_bars_two_sets(accs1, accs2, sets, labels, title, fignum, filename):
     plt.ylabel('Accuracy', size='large', fontweight='demibold')
     plt.title(title, fontweight='demibold')
     plt.xticks(ind + width, labels, size='large', fontweight='demibold')
-    plt.legend(loc='upper left', prop={'size':11})
+    plt.legend(loc='upper left', prop={'size':12})
     #plt.ylim([30,100])
     
-
-    autolabel(rects1)
-    autolabel(rects2)
+    if auto_label:
+        autolabel(rects1)
+        autolabel(rects2)
     
     #plt.show()
     plt.savefig(filename)
     
     
+def plot_bars_two_sets_stacked(word_pos_accs, char_pos_accs, word_morph_accs, char_morph_accs,  sets, labels, title, fignum, filename):
+    """ bar plot comparing two sets of results """
     
-#plot_bars_two_sets(word_pos_accs, char_pos_accs, sets, labels, 'POS Accuracy by Representation Type', 1, 'pos-acc-repr-type.png')
-#plot_bars_two_sets(word_morph_accs, char_morph_accs, sets, labels, 'Morphology Accuracy by Representation Type', 2, 'morph-acc-repr-type.png')
+    assert len(word_pos_accs) == len(char_pos_accs) and len(char_pos_accs) == len(word_morph_accs) and len(word_morph_accs) == len(char_morph_accs), 'incompatible arguments in plot_bars_two_sets'
+    
+    plt.figure(fignum)
+    ind = np.arange(len(labels))
+    width = 0.35
+    
+    rects1 = plt.bar(ind, word_pos_accs, width, color='r', hatch='/', label=sets[0])
+    rects2 = plt.bar(ind, char_pos_accs-word_pos_accs, width, bottom=word_pos_accs, color='y', hatch='/', label=sets[1])
+    rects3 = plt.bar(ind + width, word_morph_accs, width, color='r', hatch='\\', label=sets[2])
+    rects4 = plt.bar(ind + width, char_morph_accs-word_morph_accs, width, bottom=word_morph_accs, color='y', hatch='\\', label=sets[3])
+    
+    plt.ylabel('Accuracy', size='large', fontweight='demibold')
+    plt.title(title, fontweight='demibold')
+    plt.xticks(ind + width, labels, size='large', fontweight='demibold')
+    plt.legend(loc='upper left', prop={'size':12})
+    #plt.ylim([30,100])
+    
+
+    #plt.show()
+    plt.savefig(filename)    
+    
+
+def plot_bars_two_sets_ratios(word_pos_accs, char_pos_accs, word_morph_accs, char_morph_accs,  sets, labels, title, fignum, filename):
+    """ bar plot comparing two sets of results """
+    
+    assert len(word_pos_accs) == len(char_pos_accs) and len(char_pos_accs) == len(word_morph_accs) and len(word_morph_accs) == len(char_morph_accs), 'incompatible arguments in plot_bars_two_sets'
+    
+    plt.figure(fignum)
+    ind = np.arange(len(labels))
+    width = 0.35
+    
+    word_pos_errors = 100-np.array(word_pos_accs, dtype='float')
+    char_pos_errors = 100-np.array(char_pos_accs, dtype='float')
+    word_morph_errors = 100-np.array(word_morph_accs, dtype='float')
+    char_morph_errors = 100-np.array(char_morph_accs, dtype='float')
+    word_char_pos_error_reduction = (word_pos_errors - char_pos_errors) / word_pos_errors
+    word_char_morph_error_reduction = (word_morph_errors - char_morph_errors) / word_morph_errors
+    word_char_pos_absolute_acc_difference = np.array(char_pos_accs, dtype='float') - np.array(word_pos_accs, dtype='float')
+    word_char_morph_absolute_acc_difference = np.array(char_morph_accs, dtype='float') - np.array(word_morph_accs, dtype='float')
+    
+    rects1 = plt.bar(ind, word_char_pos_absolute_acc_difference, width, color='g', hatch='/', label=sets[0])
+    rects3 = plt.bar(ind + width, word_char_morph_absolute_acc_difference, width, color='c', hatch='\\', label=sets[1])
+    
+    plt.ylabel('Improvement in Accuracy', size='large', fontweight='demibold')
+    plt.title(title, fontweight='demibold')
+    plt.xticks(ind + width/2, labels, size='large', fontweight='demibold')
+    plt.legend(loc='upper right', prop={'size':14})
+    #plt.ylim([30,100])
+    plt.tight_layout()
+    
+
+    #plt.show()
+    plt.savefig(filename)    
+    
+
+    
+#plot_bars_two_sets(word_pos_accs, char_pos_accs, sets, labels, 'POS Accuracy by Representation Type', 1, 'pos-acc-repr-type.png', auto_label=False)
+#plot_bars_two_sets(word_morph_accs, char_morph_accs, sets, labels, 'Morphology Accuracy by Representation Type', 2, 'morph-acc-repr-type.png', auto_label=False)
+sets_stacked = ['Word POS', 'Char POS', 'Word Morph', 'Char Morph']
+plot_bars_two_sets_stacked(word_pos_accs, char_pos_accs, word_morph_accs, char_morph_accs, sets_stacked, labels, 'Accuracy by Representation Type', 1, 'acc-repr-type-stacked.png')
+plot_bars_two_sets_ratios(word_pos_accs, char_pos_accs, word_morph_accs, char_morph_accs, ['POS', 'Morph'], labels, 'Improvement in POS and Morphology Accuracy', 111, 'acc-repr-type-diff.png')
+
 
 
 ### effect of layer depth ###
@@ -99,6 +161,61 @@ layer_labels = ['Word-POS', 'Word-Morph', 'Char-POS', 'Char-Morph']
 layer_colors = ['r', 'y', 'g', 'c']
 layer_markers = ['o', 's', 'P', '*']
 
+
+# effect of layer depth in different languages
+ar_en_word_layer0, ar_en_word_layer1, ar_en_word_layer2 = 77.09, 81.07, 80.31
+ar_he_word_layer0, ar_he_word_layer1, ar_he_word_layer2 = 77.25, 79.40, 78.20
+de_en_word_layer0, de_en_word_layer1, de_en_word_layer2 = 91.14, 93.57, 93.54
+fr_en_word_layer0, fr_en_word_layer1, fr_en_word_layer2 = 92.08, 95.06, 94.61
+cz_en_word_layer0, cz_en_word_layer1, cz_en_word_layer2 = 76.26, 76.98, 75.71
+word_layer0_all_langs = [ar_en_word_layer0, ar_he_word_layer0, de_en_word_layer0, fr_en_word_layer0, cz_en_word_layer0]
+word_layer1_all_langs = [ar_en_word_layer1, ar_he_word_layer1, de_en_word_layer1, fr_en_word_layer1, cz_en_word_layer1]
+word_layer2_all_langs = [ar_en_word_layer2, ar_he_word_layer2, de_en_word_layer2, fr_en_word_layer2, cz_en_word_layer2]
+ar_en_word_layers = [ar_en_word_layer0, ar_en_word_layer1, ar_en_word_layer2]
+ar_he_word_layers = [ar_he_word_layer0, ar_he_word_layer1, ar_he_word_layer2]
+de_en_word_layers = [de_en_word_layer0, de_en_word_layer1, de_en_word_layer2]
+fr_en_word_layers = [fr_en_word_layer0, fr_en_word_layer1, fr_en_word_layer2]
+cz_en_word_layers = [cz_en_word_layer0, cz_en_word_layer1, cz_en_word_layer2]
+layer_labels = ['Layer 0', 'Layer 1', 'Layer 2']
+layer_sets = ['Ar-En', 'Ar-He', 'De-En', 'Fr-En', 'Cz-En']
+
+def plot_bars_layer_all_langs(accs1, accs2, accs3, sets, labels, title, fignum, filename, indices=None, legend_loc=None, opacity=1.0):
+    
+    assert len(accs1) == len(accs2) and len(accs2) == len(accs3) and len(accs3) == len(labels), 'incompatible arguments in plot_bars_four_sets'
+    
+    plt.figure(fignum)
+    ind = indices
+    if ind == None:
+        ind = np.arange(len(labels))
+    #print ind
+    width = 0.25
+    
+    rects1 = plt.bar(ind, accs1, width, color='y', hatch='/', label=sets[0], alpha=opacity)
+    rects2 = plt.bar(ind + width, accs2, width, color='r', hatch='+', label=sets[1], alpha=opacity)
+    rects3 = plt.bar(ind + width + width, accs3, width, color='g', hatch='\\', label=sets[2], alpha=opacity)
+    
+    
+    plt.ylabel('Accuracy', size='large', fontweight='demibold')
+    plt.title(title, fontweight='demibold')
+    plt.xticks(ind + width, labels, size='large', fontweight='demibold')
+    loc = legend_loc
+    if loc == None:
+        loc = 'upper left'
+    plt.legend(loc=loc, prop={'size':14})
+    plt.ylim([60,100])
+    
+
+    #autolabel(rects1)
+    #autolabel(rects2)
+    #autolabel(rects3)
+    
+    plt.tight_layout()    
+    #plt.show()    
+    plt.savefig(filename)    
+
+#plot_bars_layer_all_langs(word_layer0_all_langs, word_layer1_all_langs, word_layer2_all_langs, layer_labels, layer_sets, \
+#                            'Accuracy by Representation Layer', 3333, 'rep-layer-acc-all-langs.png')
+    
 
 def plot_bars_two_groups(group1, group2, groups, labels, title, fignum, filename):
     
@@ -141,6 +258,46 @@ def plot_lines(xs, accs, labels, colors, markers, title, fignum, filename):
 #plot_lines([0, 1, 2], layer_accs, layer_labels, layer_colors, layer_markers, 'Accuracy by Representation Layer', 33, 'rep-layer-acc-lines.png')
 
 
+def plot_bars_five_sets(accs1, accs2, accs3, accs4, accs5, sets, labels, title, fignum, filename, indices=None, legend_loc=None, opacity=1.0):
+    """ bar plot comparing four sets of results """
+    
+    assert len(accs1) == len(accs2) and len(accs2) == len(accs3) and len(accs3) == len(accs4) and len(accs4) == len(accs5) and len(accs5) == len(labels), 'incompatible arguments in plot_bars_four_sets'
+    
+    plt.figure(fignum)
+    ind = indices
+    if ind == None:
+        ind = np.arange(len(labels))
+    #print ind
+    width = 0.2
+    
+    rects1 = plt.bar(ind, accs1, width, color='r', hatch='/', label=sets[0], alpha=opacity)
+    rects2 = plt.bar(ind + width, accs2, width, color='y', hatch='O', label=sets[1], alpha=opacity)
+    rects3 = plt.bar(ind + width + width, accs3, width, color='g', hatch='+', label=sets[2], alpha=opacity)
+    rects4 = plt.bar(ind + width + width + width, accs4, width, color='c', hatch='\\', label=sets[3], alpha=opacity)
+    rects5 = plt.bar(ind + width + width + width + width, accs5, width, color='b', hatch='*', label=sets[4], alpha=opacity)
+    
+    
+    plt.ylabel('Accuracy', size='large', fontweight='demibold')
+    plt.title(title, fontweight='demibold')
+    plt.xticks(ind + width + width, labels, size='large', fontweight='demibold')
+    loc = legend_loc
+    if loc == None:
+        loc = 'upper left'
+    plt.legend(loc=loc, prop={'size':12})
+    #plt.ylim([40,95])
+    
+
+    #autolabel(rects1)
+    #autolabel(rects2)
+    #autolabel(rects3)
+    
+    plt.tight_layout()    
+    #plt.show()    
+    plt.savefig(filename)
+
+#plot_bars_five_sets(ar_en_word_layers, ar_he_word_layers, de_en_word_layers, fr_en_word_layers, cz_en_word_layers, layer_sets, layer_labels, 'Accuracy by Representation Layer', 333, 'rep-layer-acc-all-langs.png')
+
+
 ### effect of target language ###
 # uni, 2lstm500
 word_pos_he, word_pos_en, word_pos_ar, word_pos_de = 78.13, 80.21, 67.21, 78.85
@@ -176,7 +333,7 @@ he_morph_bleu = [word_morph_he, bleu_word_he, char_morph_he, bleu_char_he]
 labels3 = ['Word Morph', 'Word BLEU', 'Char Morph', 'Char BLEU']
 
 
-def plot_bars_three_sets(accs1, accs2, accs3, sets, labels, title, fignum, filename, indices=None, legend_loc=None):
+def plot_bars_three_sets(accs1, accs2, accs3, sets, labels, title, fignum, filename, indices=None, legend_loc=None, ylabel='Accuracy or BLEU'):
     """ bar plot comparing three sets of results """
     
     assert len(accs1) == len(accs2) and len(accs2) == len(accs3) and len(accs3) == len(labels), 'incompatible arguments in plot_bars_three_sets'
@@ -192,7 +349,7 @@ def plot_bars_three_sets(accs1, accs2, accs3, sets, labels, title, fignum, filen
     rects2 = plt.bar(ind + width, accs2, width, color='y', hatch='O', label=sets[1])
     rects3 = plt.bar(ind + width + width, accs3, width, color='g', hatch='+', label=sets[2])
     
-    plt.ylabel('Accuracy or BLEU', size='large', fontweight='demibold')
+    plt.ylabel(ylabel, size='large', fontweight='demibold')
     plt.title(title, fontweight='demibold')
     plt.xticks(ind + width + width/2, labels, size='large', fontweight='demibold')
     loc = legend_loc
@@ -250,7 +407,6 @@ def plot_bars_four_sets(accs1, accs2, accs3, accs4, sets, labels, title, fignum,
     #print ind
     width = 0.2
     
-    opacity=opacity
     rects1 = plt.bar(ind, accs1, width, color='r', hatch='/', label=sets[0], alpha=opacity)
     rects2 = plt.bar(ind + width, accs2, width, color='y', hatch='O', label=sets[1], alpha=opacity)
     rects3 = plt.bar(ind + width + width, accs3, width, color='g', hatch='+', label=sets[2], alpha=opacity)
@@ -263,7 +419,7 @@ def plot_bars_four_sets(accs1, accs2, accs3, accs4, sets, labels, title, fignum,
     loc = legend_loc
     if loc == None:
         loc = 'upper left'
-    plt.legend(loc=loc, prop={'size':12})
+    plt.legend(loc=loc, prop={'size':14})
     #plt.ylim([40,95])
     
 
@@ -281,8 +437,7 @@ def plot_bars_four_sets(accs1, accs2, accs3, accs4, sets, labels, title, fignum,
 # more on target language
 labels = ['POS', 'BLEU']
 sets_all = ['Ar', 'He', 'De', 'En']
-plot_bars_four_sets(ar_word_pos_bleu, he_word_pos_bleu, de_word_pos_bleu, en_word_pos_bleu, sets_all, labels, 'Effect of Target Language on POS Accuracy', 44, 'pos-acc-target-lang-all.png', legend_loc='upper right')
-#plot_bars_three_sets(ar_word_pos_bleu, he_word_pos_bleu, en_word_pos_bleu, sets, labels, 'Effect of Target Language on POS Accuracy', 4, 'pos-acc-target-lang.png')
+#plot_bars_four_sets(ar_word_pos_bleu, he_word_pos_bleu, de_word_pos_bleu, en_word_pos_bleu, sets_all, labels, 'Effect of Target Language on POS Accuracy', 44, 'pos-acc-target-lang-all.png', legend_loc='upper right')
 
 
 # order is: word layer 1, char layer 1, word layer 2, char layer 2
