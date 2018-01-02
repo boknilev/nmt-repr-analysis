e7fb16f09347c8fd7bd41208d228b0e756be308d
Adam Poliak <azpoliak@gmail.com>
2018-01-02 13:31:52 -0500

    print out entailment labels: fixed issue related to how we store classifier_out in entailment situations that prevented the predicted labels from being stored and printed correctly

--- src/eval.lua
+++ src/eval.lua
@@ -439,8 +439,10 @@ function eval_entailment(data, epoch, logger, test_or_val, pred_filename)
     end
     local classifier_out = classifier:forward(classifier_input)
     if pred_file then
-      local _, pred_idx =  classifier_out:max(1)
-      pred_idx = pred_idx:long()[1]
+      local pred_idx = 1
+      if classifier_out[{1, 1}] < classifier_out[{1, 2}] then
+        pred_idx = 2
+      end
       local pred_label = idx2label[pred_idx]
       table.insert(pred_labels, pred_label)
     end
