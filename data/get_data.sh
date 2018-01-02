0a73cc6249f1cd19d09d21d07f433d63bd986ec6
Adam Poliak <azpoliak@gmail.com>
2018-01-02 16:11:35 -0500

    script to get and reformat snli

--- data/get_data.sh
+++ data/get_data.sh
@@ -6,3 +6,10 @@ rm inference_is_everything.zip
 cd ../
 echo "About to split the data into formats for train.lua and eval.lua"
 python split-data.py
+
+echo "Downloading SNLI"
+wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip
+unzip snli_1.0.zip
+
+echo "Reformatting SNLI dataset"
+python convert_snli.py
