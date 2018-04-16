3eee5be35ff3061d962648c1163a798fb1d243e5
Adam Poliak <azpoliak@gmail.com>
2018-04-16 00:59:39 -0400

    utils: analysis-sent-length for NLI. Made it more robust

--- utils/analysis-sentence_length.py
+++ utils/analysis-sentence_length.py
@@ -1,12 +1,11 @@
 import itertools
-import pdb
 import argparse
 import numpy as np
 
 def get_sent_len_stats_test():
   for dataset in ["fnplus", "sprl", "dpr"]:
     texts, hypts = [], []
-    for line in open("/export/ssd/apoliak/nmt-repr-anaysis-sem/data/rte/cl_%s_test_source_file" % (dataset)):
+    for line in open("/export/ssd/apoliak/nmt-repr-anaysis-sem/data/rte/cl_%s_val_source_file" % (dataset)):
       pair = line.split("|||")
       texts.append(len(pair[0].strip().split()))
       hypts.append(len(pair[1].strip().split()))
@@ -47,21 +46,28 @@ def get_data(gold_f, src_f, ar_pred_f, es_pred_f, zh_pred_f, de_pred_f):
 
  return right, wrong
 
-def acc_short_sents(pred_f, gold_f, src_f):
+def acc_short_sents(pred_f, gold_f, src_f, lang):
   #ar_pred = open("/export/ssd/apoliak/nmt-repr-anaysis-sem/classifiers/fnplus/UN/en-ar-4layers-brnn-enclayer4/linear_classifier/pred_file.epoch1")
-  pred_es = open(pred_f)
-  gold=open(gold_f)
-  sents=open(src_f)
-
-  corr, tot = 0,0
-  for sent, gold, pred in itertools.izip(sents, gold, pred_es): #, pred_es, pred_zh, pred_de)):
-    if len(sent.split("|||")[0].split()) > 15:
-      continue
-    if gold == pred:
-      corr += 1
-    tot +=1
-
-  print "Accuracy: %f\t tot: %d" % (corr/float(tot), tot)
+  print lang
+  res = {}
+  for sent_lens in [0,10,20,30,40,50,60,70]: #,80]: #,90,100,110]:
+    corr, tot = 0,0
+    pred_es = open(pred_f)
+    gold=open(gold_f)
+    sents=open(src_f)
+    for sent, gold, pred in itertools.izip(sents, gold, pred_es): #, pred_es, pred_zh, pred_de)):
+      curr_len = len(sent.split("|||")[0].split())
+      if curr_len < sent_lens:
+        continue
+      if curr_len >= sent_lens + 10:
+        continue
+      if gold.strip() == pred.strip():
+        corr += 1
+      tot +=1
+    if tot > 0:
+      res["%d-%d" % (sent_lens, sent_lens+10)] = (corr/float(tot), tot)
+      print "Sentence length:%d-%d\tAccuracy: %f\t tot: %d" % (sent_lens, sent_lens+10, corr/float(tot), tot)
+  return res
 
 def get_source_sents(src_f):
   src = {}
@@ -75,11 +81,11 @@ def get_source_sents(src_f):
   return src
 
 
-def main():
+def main(args):
   get_sent_len_stats_test()
 
-  srcs = get_source_sents()
-  pdb.set_trace()
+  srcs = get_source_sents(args.src)
+  '''
   right, wrong = get_data() #args.src, args.gold, args.pred)
   right_loc, wrong_loc = {}, {}
 
@@ -116,8 +122,25 @@ def main():
 
   print "Correctly predicted entailed pos: %f" % (np.mean(right_loc['entailed']))
   print "Correctly Predicted not-entailed pos: %f" % (np.mean(right_loc['not-entailed']))
+  '''
 
-  acc_short_sents()
+  langs2stats = {}
+  for pair in [(args.ar_pred, "ar"), (args.es_pred, "es"), (args.zh_pred, "zh"), (args.de_pred, "de")]: #, (args.zh_pred, "zh"), (args.es_pred, "es")]:
+    tmp = acc_short_sents(pair[0], args.gold, args.src, pair[1])
+    if pair[1] not in langs2stats:
+      langs2stats[pair[1]] = {}
+    langs2stats[pair[1]] =  tmp
+
+  latex = ""
+  keys = langs2stats[langs2stats.keys()[0]].keys()
+  keys.sort()
+  for key in keys:
+    latex += "\small{%s} & " % (key)
+    for lang in langs2stats:
+      latex += " \small{%.1f} & " % (100*langs2stats[lang][key][0])
+    latex += "\small{%d} \\\\ \n" % (langs2stats[lang][key][1]) 
+  #latex += "\\n \\"
+  print latex
 
 if __name__ == '__main__':
 
@@ -133,5 +156,5 @@ if __name__ == '__main__':
 
   args = parser.parse_args()
 
-  main()
+  main(args)
 
